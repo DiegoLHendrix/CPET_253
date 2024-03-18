288aff48c51c39d060ad0e80165b99f348df22e2
DiegoLHendrix <119289719+DiegoLHendrix@users.noreply.github.com>
2024-03-18 16:10:45 -0400

    Lab 6

---
+++ Lab6_BLE/Debug/Lab6_BLEmain.d
@@ -0,0 +1,87 @@
+# FIXED
+
+Lab6_BLEmain.obj: ../Lab6_BLEmain.c
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h
+Lab6_BLEmain.obj: ../../inc/Clock.h
+Lab6_BLEmain.obj: ../../inc/CortexM.h
+Lab6_BLEmain.obj: ../../inc/AP.h
+Lab6_BLEmain.obj: ../../inc/UART0.h
+Lab6_BLEmain.obj: ../../inc/UART1.h
+Lab6_BLEmain.obj: ../../inc/SSD1306.h
+Lab6_BLEmain.obj: ../../inc/motor.h
+Lab6_BLEmain.obj: ../../inc/Init_Ports.h
+Lab6_BLEmain.obj: ../../inc/Init_Timers.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/msp.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/msp432p401r.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/msp_compatibility.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/msp432p401r_classic.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Lab6_BLEmain.obj: C:/ti/ccs1110/ccs/ccs_base/arm/include/system_msp432p401r.h
+
+../Lab6_BLEmain.c:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_ti_config.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/linkage.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/_stdint40.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_types.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_types.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1110/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS/include/sys/_stdint.h:
+
+../../inc/Clock.h:
+
+../../inc/CortexM.h:
+
+../../inc/AP.h:
+
+../../inc/UART0.h:
+
+../../inc/UART1.h:
+
+../../inc/SSD1306.h:
+
+../../inc/motor.h:
+
+../../inc/Init_Ports.h:
+
+../../inc/Init_Timers.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1110/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
---
+++ Lab6_BLE/Init_Ports.c
@@ -0,0 +1,234 @@
+// Init_Ports.c
+// Runs on MSP432
+// Provide functions that initialize ports 
+// Prof. Christman
+// 1/8/2023
+
+/* These functions will be used in all of the labs for CPET253
+
+There is an initialization function for each of the ports 1-10 and J.
+
+The initializations will be different for each lab.
+*/
+
+#include "msp.h"
+
+
+// ------------Port1_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if appliable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port1_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+
+    P1DIR &= ~0x12;//Turns pins 1 and 4 into inputs
+    P1REN |= 0x12;//Enables pull up/down resistors for pins 1 and 4
+    P1OUT |= 0x12;//Specifies for pull up resistors
+
+}
+
+// ------------Port2_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if appliable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port2_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+
+    P2DIR |= 0x07;//Setting P2.0/1/2 (red, green, blue respectively) as outputs
+    P2DIR |= 0xC0;//bits 7 and 6 set as outputs
+    //SEL = 01(SEL1, SEL0) to activate secondary module function
+    P2SEL1 &= ~0xC0;//~1100 0000
+    P2SEL0 |= 0xC0;//1100 0000
+}
+
+// ------------Port3_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port3_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+
+    P3DIR |= 0xC0;//1100 0000. Set left and right motor sleep as outputs
+}
+
+// ------------Port4_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port4_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+
+    //set port 4 pins as interrupt sources
+    P4DIR &= ~0xED;//set pins to inputs 1110 1101
+    P4REN |= 0xED;//enable resistors
+    P4OUT |= 0xED;//specify pull up resistors
+
+}
+
+// ------------Port5_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port5_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+
+    P5DIR |= 0x30;//0011 0000. Set left and right motor direction as outputs
+}
+
+// ------------Port6_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port6_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+}
+
+// ------------Port7_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port7_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+}
+
+// ------------Port8_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port8_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+}
+
+// ------------Port9_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port9_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+}
+
+// ------------Port10_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void Port10_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+}
+
+// ------------PortJ_Init------------
+// First set the pin directions
+// Enable resistors for inputs
+// Set input resistors for pull-up or pull-down
+// Enable alternate pin functions, if applicable
+// Drive initial values on outputs
+// Input: none
+// Output: none
+void PortJ_Init(void){
+  // Use the port direction register to configure pins as inputs and output
+  // 	-A 1 sets a pin as an output. Use |= with a bit mask to set 1's.
+  // 	-A 0 sets a pin as an input. Use &= with a bit mask to set 0's.
+  // Use the resistor enable register to enable resistors for inputs that need them
+  // Use the port output register to configure the resistors as pull-ups or pull-downs
+  // Use the PxSEL0 and PxSEL1 registers to enable alternate pin functions, if applicable
+  // Use the port output register to put outputs to an initial state
+}
